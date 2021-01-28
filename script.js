638fcf7a131132c0631aacc300f506b7d8702f86
jcxiao <chang.x1994@gmail.com>
2021-01-28 15:21:41 -0500

    fixed issue with highlighting text

--- script.js
+++ script.js
@@ -18,46 +18,37 @@ Object.entries(taskData).forEach(task => {
 })
 
 // track all clicks on the document
-$(document).on('click', newSelection );
+$(document).on('mousedown', newSelection );
 
 let currentDiv = '';
 function newSelection(event) {
-    // if you click on a new div
+    event.stopPropagation();
     if (currentDiv !== event.target.id) {
         // save stuff for previous selection
         saveData(currentDiv);
         // track new selection
         currentDiv = event.target.id;
     }
-
 }
 
 function saveData(id) {
     // check if id being selected is not blank
     if (id !== '') {
-        if ($('#'+id).val() === '') {
-            console.log('removing data for ' + id);
-            delete taskData[id];
-        }
-        else {
-            console.log('saving data for ' + id);
-            taskData[id] = $('#'+id).val();
-        }
+        if ($('#'+id).val().length) taskData[id] = $('#'+id).val();
+        // delete data from taskData if the entry is blank
+        else delete taskData[id];
         localStorage.taskData = JSON.stringify(taskData);
     }
 }
 
-// modify background-color of all blocks to match day
 let currentHr = parseInt(moment().format('HH')); //24 hr format
 let percentOfHr = Math.round((parseInt(moment().format('mm'))/60)*100); //0 to 100
 
+// modify background-color of all blocks to match time of day
 for (let i=6; i<21; i++) {
     if ($('#row-'+i).length) {
-        // before current hour
         if (i < currentHr) $('#row-'+i).css('background-color','rgb(250, 190, 190)');
-        // current hour
         if (i === currentHr) $('#row-'+i).css('background',`linear-gradient(180deg, rgb(250, 190, 190) ${percentOfHr-10}%, rgb(190, 250, 190) ${percentOfHr+10}%)`);
-        // after current hour
         if (i > currentHr) $('#row-'+i).css('background-color','rgb(190, 250, 190)');
     }
 }
