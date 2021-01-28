a1114c20fd2e1cc6d0625385129adedb5f35fad5
jcxiao <chang.x1994@gmail.com>
2021-01-28 11:05:57 -0500

    Added basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# UTOR-Scheduler
-A simple day scheduler that uses jQuery to implement interactivity
+# 05 Third-Party APIs: Work Day Scheduler
+
+This is a simple day planner made in HTML and JS, taking advantage
+of jQuery and MomentJS to perform API functions and handle date/time
+information. 
+
+## Link to live website
+http://sinsinkun.github.io/UTOR-Scheduler
+
+## Screenshot
+![Screenshot](./screenshot.png)
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
---
+++ script.js
@@ -0,0 +1,65 @@
+// Add day to top
+$('#day-display').html(moment().format('MMMM Do YYYY'));
+
+// retrieve existing scheduler data
+let taskData = {day: moment().format('MMMM Do YYYY')};
+let storedTaskData = localStorage.taskData ? JSON.parse(localStorage.taskData) : {};
+// retrieve existing data only if same day
+if (taskData.day === storedTaskData.day) {
+    taskData = storedTaskData;
+}
+// remove existing data if it's from a different day
+else if (storedTaskData.length > 0) localStorage.removeItem('taskData');
+
+// fill in scheduler with data
+Object.entries(taskData).forEach(task => {
+    let [id, input] = task;
+    $('#'+id).val(input);
+})
+
+// track all clicks on the document
+$(document).on('click', newSelection );
+
+let currentDiv = '';
+function newSelection(event) {
+    // if you click on a new div
+    if (currentDiv !== event.target.id) {
+        // save stuff for previous selection
+        saveData(currentDiv);
+        // track new selection
+        currentDiv = event.target.id;
+    }
+
+}
+
+function saveData(id) {
+    // check if id being selected is not blank
+    if (id !== '') {
+        if ($('#'+id).val() === '') {
+            console.log('removing data for ' + id);
+            delete taskData[id];
+        }
+        else {
+            console.log('saving data for ' + id);
+            taskData[id] = $('#'+id).val();
+        }
+        localStorage.taskData = JSON.stringify(taskData);
+    }
+}
+
+// modify background-color of all blocks to match day
+let currentHr = parseInt(moment().format('HH')); //24 hr format
+let percentOfHr = 100-Math.round((parseInt(moment().format('mm'))/60)*100); //0 to 100
+
+for (let i=6; i<21; i++) {
+    if ($('#row-'+i).length) {
+        // before current hour
+        if (i < currentHr) {
+            $('#row-'+i).css('background-color','rgb(250, 190, 190)');
+        }
+        // current hour
+        if (i === currentHr) $('#row-'+i).css('background',`linear-gradient(0deg, rgb(190, 250, 190) ${percentOfHr}%, rgb(250, 190, 190) ${20-percentOfHr}%)`);
+        // after current hour
+        if (i > currentHr) $('#row-'+i).css('background-color','rgb(190, 250, 190)');
+    }
+}
